48408740027542d8eaa438a4c9712e4c61b34a3b
sato-kai <h175bkznpicloud.com>
2022-01-21 23:56:24 +0900

    [add]記事ページのサイドバー作成

---
+++ src/components/article/SideBar.tsx
@@ -0,0 +1,25 @@
+import { memo, VFC } from "react";
+import { Link } from "react-router-dom";
+import { SideBar } from "../SideBar";
+
+type props = {
+  isVisible?: 'top' | 'create';
+}
+
+export const ArticleSideBar: VFC<props> = memo((props) => {
+  const { isVisible } = props;
+
+  return (
+    <SideBar>
+      <Link className={isVisible === 'top' ? 'is-active' : ''} to="/article/">
+        記事TOP
+      </Link>
+      <Link className={isVisible === 'create' ? 'is-active' : ''} to="/article/create/">
+        記事作成
+      </Link>
+      <Link to="#" onClick={() => alert('まだ未実装デース')}>
+        記事検索
+      </Link>
+    </SideBar>
+  );
+})
