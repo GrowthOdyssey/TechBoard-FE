378a3da7eee6b08540b296b281e67de3b7671356
sato-kai <h175bkznpicloud.com>
2022-02-10 22:53:00 +0900

    [update]記事作成ロジックをuseArticlesに記載

--- src/pages/article/Create.tsx
+++ src/pages/article/Create.tsx
@@ -2,22 +2,32 @@ import { VFC, memo, ChangeEvent, useState } from 'react';
 import styled from 'styled-components';
 import { MarkDownEditor } from '../../components/article/MarkDownEditor';
 import { ArticleSideBar } from '../../components/article/SideBar';
+import { Button } from '../../components/common/Button';
 import { Heading } from '../../components/common/Heading';
 import { TextInput } from '../../components/common/TextInput';
 import { Contents } from '../../components/Contents';
+import { useArticle } from '../../hooks/useArticle';
+import { useLoginUser } from '../../providers/LoginUserProvider';
 import { palette } from '../../variable';
 
 export const ArticleCreate: VFC = memo(() => {
   const [title, setTitle] = useState('');
   const [markdown, setMarkdown] = useState('');
+  const { loginUser } = useLoginUser();
+  const { createArticle } = useArticle();
+
   const onchangeTitle = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
-  const onChangeText = (value: string) => setMarkdown(value);
+  const onchangeText = (value: string) => setMarkdown(value);
+  const onclickCreateArticle = () => {
+    createArticle(loginUser.accessToken, title, markdown, loginUser.userId);
+  };
 
   return (
     <>
       <ArticleSideBar isVisible={'create'} />
       <Contents>
         <Heading size={'2'}>記事新規作成</Heading>
+        {!loginUser.userId && <p>記事を作成するにはログインして下さい。</p>}
         <_Form>
           <Heading size={'4'}>Title</Heading>
           <TextInput
@@ -26,7 +36,12 @@ export const ArticleCreate: VFC = memo(() => {
             onChange={onchangeTitle}
           />
           <Heading size={'4'}>記事内容</Heading>
-          <MarkDownEditor isEdit value={markdown} onChange={onChangeText} />
+          <MarkDownEditor isEdit value={markdown} onChange={onchangeText} />
+          <Button
+            label={'作成'}
+            onclick={onclickCreateArticle}
+            isDisabled={loginUser.userId ? false : true}
+          />
         </_Form>
       </Contents>
     </>
@@ -44,6 +59,7 @@ const _Form = styled.div`
     margin-bottom: 20px;
   }
   .btn {
-    margin-top: 40px;
+    display: block;
+    margin: 40px auto 0;
   }
 `;
