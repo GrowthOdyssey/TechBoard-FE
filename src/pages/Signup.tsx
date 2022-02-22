c6c53d3821b12560f71d4eb37e28d9f198ace243
sato-kai <h175bkznpicloud.com>
2022-02-23 00:42:17 +0900

    [fix]ログイン済みで会員登録画面を表示しようとした場合トップにリダイレクトさせる

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,16 +1,26 @@
-import { VFC, memo, useState, ChangeEvent } from 'react';
+import { VFC, memo, useState, ChangeEvent, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../components/common/Button';
 import { Form } from '../components/common/Form';
 import { Heading } from '../components/common/Heading';
 import { TextInput } from '../components/common/TextInput';
+import { useRedirect } from '../hooks/useRedirect';
 import { useUser } from '../hooks/useUser';
+import { useLoginUser } from '../providers/LoginUserProvider';
 
 export const Signup: VFC = memo(() => {
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [userName, setUserName] = useState('');
   const { signup } = useUser();
+  const { loginUser } = useLoginUser();
+  const { toTop } = useRedirect();
+  
+  useEffect(() => {
+    if (loginUser.userId) {
+      toTop();
+    }
+  }, [])
 
   const onchangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
   const onchangePassword = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
