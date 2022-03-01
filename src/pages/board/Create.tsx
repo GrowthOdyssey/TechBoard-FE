a2e52332da5b2069854bd42c98911ebf0328f67a
hiro0214 <leonhurt2@gmail.com>
2022-03-01 12:31:32 +0900

    [update] Board エラーテキストの表示

--- src/pages/board/Create.tsx
+++ src/pages/board/Create.tsx
@@ -2,6 +2,7 @@ import { VFC, memo, useState, ChangeEvent, useEffect } from 'react';
 import styled from 'styled-components';
 import { BoardSideBar } from '../../components/board/SideBar';
 import { Button } from '../../components/common/Button';
+import { ErrorText } from '../../components/common/ErrorText';
 import { Heading } from '../../components/common/Heading';
 import { Select } from '../../components/common/Select';
 import { TextInput } from '../../components/common/TextInput';
@@ -15,11 +16,11 @@ export const BoardCreate: VFC = memo(() => {
   const { loginUser } = useLoginUser();
   const [category, setCategory] = useState<categoryType>({} as categoryType);
   const [title, setTitle] = useState('');
-  const { categories, createThread, getCategories } = useBoard();
+  const { categories, errorText, createThread, getCategories } = useBoard();
 
   useEffect(() => {
     getCategories();
-  }, []);
+  }, [errorText]);
 
   const onchangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
     const selectedIndex = e.target.selectedIndex;
@@ -27,14 +28,7 @@ export const BoardCreate: VFC = memo(() => {
   };
   const onchangeTitle = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
   const getCategory = () => categories.map((v) => v.categoryName);
-  const onclickCreateThread = () => {
-    if (category.categoryId && title !== '') {
-      createThread(loginUser.accessToken, category.categoryId, title);
-    } else {
-      if (!category.categoryId) alert('カテゴリーを選択して下さい');
-      else if (title === '') alert('スレッドタイトルを入力して下さい。');
-    }
-  };
+  const onclickCreateThread = () => createThread(loginUser.accessToken, category.categoryId, title);
 
   return (
     <>
@@ -42,6 +36,7 @@ export const BoardCreate: VFC = memo(() => {
       <Contents>
         <Heading size={'2'}>スレッド新規作成</Heading>
         {!loginUser.userId && <p>スレッドを作成するにはログインして下さい。</p>}
+        {errorText.length ? <ErrorText errors={errorText} /> : ''}
         <_Form className={!loginUser.userId ? 'is-disabled' : ''}>
           <Select options={getCategory()} name={'category'} onChange={onchangeCategory} hdg={'カテゴリー'} />
           <TextInput value={title} placeholder={'スレッド名を入力してください'} onChange={onchangeTitle} />
