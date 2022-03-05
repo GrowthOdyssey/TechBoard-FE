87bf717ac276f5402fd20d479c9130ea7e8b35ac
sato-kai <h175bkznpicloud.com>
2022-03-05 13:10:34 +0900

    [fix]mypageリファクタ

--- src/pages/article/MyPage.tsx
+++ src/pages/article/MyPage.tsx
@@ -1,5 +1,4 @@
 import { VFC, memo, useEffect } from 'react';
-import { ArticleCard } from '../../components/article/Card';
 import { ArticleSideBar } from '../../components/article/SideBar';
 import { Heading } from '../../components/common/Heading';
 import { Loading } from '../../components/common/Loading';
@@ -7,6 +6,7 @@ import { Contents } from '../../components/Contents';
 import { useArticle } from '../../hooks/useArticle';
 import { useRedirect } from '../../hooks/useRedirect';
 import { useLoginUser } from '../../providers/LoginUserProvider';
+import { ArticleList } from './List';
 
 export const ArticleMyPage: VFC = memo(() => {
   const { articleList, loading, getArticleList } = useArticle();
@@ -22,7 +22,7 @@ export const ArticleMyPage: VFC = memo(() => {
 
   return (
     <>
-      <ArticleSideBar isVisible={loginUser.userId ? 'mypage' : 'top'} />
+      <ArticleSideBar isVisible={'mypage'} />
       {loading ? (
         <Loading />
       ) : (
@@ -30,11 +30,7 @@ export const ArticleMyPage: VFC = memo(() => {
           <Heading size={'2'}>{loginUser.userName + 'の投稿した記事一覧'}</Heading>
           <section>
             <Heading size={'4'}>最新の記事一覧</Heading>
-            <ul className="list">
-              {articleList.map((article) => (
-                <ArticleCard key={article.articleId} data={article} />
-              ))}
-            </ul>
+            <ArticleList articleList={articleList} />
           </section>
         </Contents>
       )}
